054e3fa28f2b888867fea706cd9d93d9b352fc16
James Howard <james@outoftolerance.com>
2019-06-11 20:43:53 -0700

    Arduino PWM Driver + Test Code

--- hab_tracker.ino
+++ hab_tracker.ino
@@ -1,18 +1,26 @@
-#include <Servo.h>
+#include <Wire.h>
+#include <Math.h>
 
+#include <Adafruit_PWMServoDriver.h>
 #include <Timer.h>
 #include <Log.h>
 #include <SimpleHDLC.h>
 #include <TinyGPS++.h>
 
-#define PAN_SERVO 2
-#define TILT_SERVO 3
+#define PAN_SERVO 0
+#define TILT_SERVO 1
 
-Servo pan_servo;	/*< Pan servo object */
-Servo tilt_servo;	/*< Tilt servo object */
+#define SERVO_PWM_FREQUENCY 60  // Analog servos run at ~60 Hz updates
 
-Stream& logging_output_stream = Serial;             /**< Logging output stream, this is of type Serial_ */
-Stream& command_input_stream = Serial;              /**< Message and command interface stream */
+#define PAN_SERVO_PWM_MIN  150  // this is the 'minimum' pulse length count (out of 4096)
+#define PAN_SERVO_PWM_MAX  500  // this is the 'maximum' pulse length count (out of 4096)
+#define TILT_SERVO_PWM_MIN  258 // this is the 'minimum' pulse length count (out of 4096)
+#define TILT_SERVO_PWM_MAX  480 // this is the 'maximum' pulse length count (out of 4096)
+
+Stream& logging_output_stream = Serial;                     /**< Logging output stream, this is of type Serial_ */
+Stream& command_input_stream = Serial;                      /**< Message and command interface stream */
+
+Adafruit_PWMServoDriver servo_driver = Adafruit_PWMServoDriver();    /**< Adafruit servo driver object */
 
 enum MESSAGE_TYPES {
     MESSAGE_TYPE_REPORT_TRACKER_POINT,
@@ -43,12 +51,12 @@ void setup() {
     //Sleep 5s so that debug can connect
     delay(5000);
 
-    //Set servo pins
-    pan_servo.attach(PAN_SERVO);
-    tilt_servo.attach(TILT_SERVO);
-
     //Start serial port to command PC
     Serial.begin(57600);
+
+    //Initialize PWM driver
+    servo_driver.begin();
+    servo_driver.setPWMFreq(SERVO_PWM_FREQUENCY);
 }
 
 /**
@@ -71,6 +79,16 @@ void loop() {
 			//Get current bearing
 
 			//Move pan until at bearing
+
+        servo_driver.setPWM(PAN_SERVO, 0, PAN_SERVO_PWM_MIN);
+        servo_driver.setPWM(TILT_SERVO, 0, TILT_SERVO_PWM_MIN);
+
+        delay(1000);
+
+        servo_driver.setPWM(PAN_SERVO, 0, PAN_SERVO_PWM_MAX);
+        servo_driver.setPWM(TILT_SERVO, 0, TILT_SERVO_PWM_MAX);
+
+        delay(1000);
 	}
 }
 
@@ -78,7 +96,7 @@ float azimuthTo(float base_altitude, float target_horizontal_distance, float tar
 {
 	float altitude_difference = target_altitude - base_altitude;
 
-	return arctan(altitude_difference, target_horizontal_distance);
+	return atan2(altitude_difference, target_horizontal_distance);
 }
 
 void handleMessageCallback(hdlcMessage message)
@@ -87,29 +105,9 @@ void handleMessageCallback(hdlcMessage message)
 
     switch(message.command)
     {
-        case MESSAGE_TYPES::MESSAGE_TYPE_REPORT_TELEMETRY:
-            logger.event(LOG_LEVELS::INFO, "Received telemetry report.");
-            handleMessageTelemetryReport(message);
-            break;
-        case MESSAGE_TYPES::MESSAGE_TYPE_REPORT_POSITION:
-            logger.event(LOG_LEVELS::INFO, "Received position report.");
-            handleMessagePositionReport(message);
-            break;
-        case MESSAGE_TYPES::MESSAGE_TYPE_COMMAND_ARM:
-            logger.event(LOG_LEVELS::INFO, "Received takeoff command.");
-            handleMessageCommandArm(message);
-            break;
-        case MESSAGE_TYPES::MESSAGE_TYPE_COMMAND_DISARM:
-            logger.event(LOG_LEVELS::INFO, "Received abort takeoff message.");
-            handleMessageCommandDisarm(message);
-            break;
-        case MESSAGE_TYPES::MESSAGE_TYPE_COMMAND_SET_STATE:
-            logger.event(LOG_LEVELS::INFO, "Received set state command.");
-            handleMessageCommandSetState(message);
-            break;
         case MESSAGE_TYPES::MESSAGE_TYPE_PROTO_ACK:
             logger.event(LOG_LEVELS::INFO, "Received acknowledgement.");
-            handleMessageProtoAck(message);
+            //handleMessageProtoAck(message);
             break;
     }
 }
