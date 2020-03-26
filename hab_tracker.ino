af4fcfc1876075a321bbef2a77966d2721060e9b
James Howad <james@outoftolerance.com>
2020-03-25 22:12:16 -0700

    Fixes some basic errors, starts adding servo control.

--- hab_tracker.ino
+++ hab_tracker.ino
@@ -15,9 +15,9 @@
 #define SERVO_PWM_FREQUENCY 60  // Analog servos run at ~60 Hz updates
 
 #define PAN_SERVO_PWM_MIN  150  // this is the 'minimum' pulse length count (out of 4096)
-#define PAN_SERVO_PWM_MAX  500  // this is the 'maximum' pulse length count (out of 4096)
-#define TILT_SERVO_PWM_MIN  258 // this is the 'minimum' pulse length count (out of 4096)
-#define TILT_SERVO_PWM_MAX  480 // this is the 'maximum' pulse length count (out of 4096)
+#define PAN_SERVO_PWM_MAX  600  // this is the 'maximum' pulse length count (out of 4096)
+#define TILT_SERVO_PWM_MIN  262 // this is the 'minimum' pulse length count (out of 4096)
+#define TILT_SERVO_PWM_MAX  488 // this is the 'maximum' pulse length count (out of 4096)
 
 Stream& logging_output_stream = Serial;                     /**< Logging output stream, this is of type Serial_ */
 Stream& command_input_stream = Serial;                      /**< Message and command interface stream */
@@ -31,6 +31,9 @@ Adafruit_PWMServoDriver servo_driver = Adafruit_PWMServoDriver();    /**< Adafru
  */
 void handleMessageCallback(hdlcMessage message);
 
+float calcAzimuthTo(float base_altitude, float target_horizontal_distance, float target_altitude);
+void stop();
+
 SimpleHDLC usb(command_input_stream, &handleMessageCallback);        /**< HDLC messaging object, linked to message callback */
 Log logger(logging_output_stream, LOG_LEVELS::DEBUG);                /**< Log object */
 Telemetry telemetry(IMU_TYPES::IMU_TYPE_ADAFRUIT_9DOF);              /**< Telemetry object */
@@ -43,8 +46,8 @@ Timer timer_telemetry_check;          /**< Timer sets interval between telemetry
  * @details Initialises all system componenets at start-up
  */
 void setup() {
-    //Sleep 5s so that debug can connect
-    delay(5000);
+    //Sleep until debug can connect
+    while(!Serial);
 
     //Setup pin modes
     pinMode(LED_BUILTIN, OUTPUT);
@@ -56,19 +59,24 @@ void setup() {
     //Start command interface over USB
     logger.event(LOG_LEVELS::INFO, "Starting USB serial interface...");
     //static_cast<HardwareSerial&>(command_input_stream).begin(57600);
+    logger.event(LOG_LEVELS::INFO, "Done!");
 
     //Initialise the telemetry system
     logger.event(LOG_LEVELS::INFO, "Initialising Telemetry subsystem...");
     if(!telemetry.init())
     {
         logger.event(LOG_LEVELS::FATAL, "Failed to initialise Telemetry subsystem!");
-        while(1);
+        stop();
     }
+    logger.event(LOG_LEVELS::INFO, "Done!");
 
     //Initialize PWM driver
     logger.event(LOG_LEVELS::INFO, "Starting PWM Driver...");
     servo_driver.begin();
     servo_driver.setPWMFreq(SERVO_PWM_FREQUENCY);
+    logger.event(LOG_LEVELS::INFO, "Done!");
+
+    logger.event(LOG_LEVELS::INFO, "Finished initialisation, starting program!");
 }
 
 /**
@@ -76,16 +84,18 @@ void setup() {
  * @details Called after setup() function, loops inifiteley, everything happens here
  */
 void loop() {
-    timer_execution_led.setInterval(500);                   /**< Sets execution blinky LED interval to 500ms */
-    timer_execution_led.start();
+    timer_execution_led.setInterval(1000);
     timer_telemetry_check.setInterval(100);
-    timer_telemetry_check.start();
+    
     TelemetryStruct current_telemetry;                      /**< Current telemetry */
+
+    timer_execution_led.start();
+    timer_telemetry_check.start();
 
 	while(1)
 	{
 		//Get messages from command interface
-        usb.receive();
+        //usb.receive();
 
         //Telemetry Update
         if(timer_telemetry_check.check())
@@ -107,12 +117,11 @@ void loop() {
 		//Check if base location is set yet
 
 			//Calculate bearing and azimuth to target
-            //target_bearing = ;
-            //target_azimuth = azimuthTo(tracker.pose.altitude, target.);
-
-			//Get current tilt
+            float target_azimuth = calcAzimuthTo(0, 1, 1) - current_telemetry.roll + 90;
 
 			//Move tilt until at azimuth
+            int tilt_pulse = map(target_azimuth, 0, 90, TILT_SERVO_PWM_MIN, TILT_SERVO_PWM_MAX);
+            servo_driver.setPWM(TILT_SERVO, 0, tilt_pulse);
 
 			//Get current bearing
 
@@ -129,25 +138,25 @@ void loop() {
             {
                 digitalWrite(LED_BUILTIN, HIGH);
             }
+
+            //Print a bunch of debug information
+            logger.event(LOG_LEVELS::DEBUG, "Current GPS Latitude   ", current_telemetry.latitude);
+            logger.event(LOG_LEVELS::DEBUG, "Current GPS Longitude  ", current_telemetry.longitude);
+            logger.event(LOG_LEVELS::DEBUG, "Current GPS Altitude   ", current_telemetry.altitude);
+            logger.event(LOG_LEVELS::DEBUG, "Current GPS Course     ", current_telemetry.course);
+            logger.event(LOG_LEVELS::DEBUG, "Current IMU Roll       ", current_telemetry.roll);
+            logger.event(LOG_LEVELS::DEBUG, "Current IMU Pitch      ", current_telemetry.pitch);
+            logger.event(LOG_LEVELS::DEBUG, "Current IMU Heading    ", current_telemetry.heading);
+            logger.event(LOG_LEVELS::DEBUG, "Current IMU Altitude   ", current_telemetry.altitude_barometric);
+            logger.event(LOG_LEVELS::DEBUG, "Current IMU Pressure   ", current_telemetry.pressure);
+            logger.event(LOG_LEVELS::DEBUG, "Current IMU Temperature", current_telemetry.temperature);
+
+            timer_execution_led.reset();
         }
-
-        //Print a bunch of debug information
-        logger.event(LOG_LEVELS::DEBUG, "Current GPS Latitude   ", current_telemetry.latitude);
-        logger.event(LOG_LEVELS::DEBUG, "Current GPS Longitude  ", current_telemetry.longitude);
-        logger.event(LOG_LEVELS::DEBUG, "Current GPS Altitude   ", current_telemetry.altitude);
-        logger.event(LOG_LEVELS::DEBUG, "Current GPS Course     ", current_telemetry.course);
-        logger.event(LOG_LEVELS::DEBUG, "Current IMU Roll       ", current_telemetry.roll);
-        logger.event(LOG_LEVELS::DEBUG, "Current IMU Pitch      ", current_telemetry.pitch);
-        logger.event(LOG_LEVELS::DEBUG, "Current IMU Heading    ", current_telemetry.heading);
-        logger.event(LOG_LEVELS::DEBUG, "Current IMU Altitude   ", current_telemetry.altitude_barometric);
-        logger.event(LOG_LEVELS::DEBUG, "Current IMU Pressure   ", current_telemetry.pressure);
-        logger.event(LOG_LEVELS::DEBUG, "Current IMU Temperature", current_telemetry.temperature);
-
-        delay(500);
 	}
 }
 
-float azimuthTo(float base_altitude, float target_horizontal_distance, float target_altitude)
+float calcAzimuthTo(float base_altitude, float target_horizontal_distance, float target_altitude)
 {
 	float altitude_difference = target_altitude - base_altitude;
 
@@ -164,5 +173,16 @@ void handleMessageCallback(hdlcMessage message)
             logger.event(LOG_LEVELS::INFO, "Received acknowledgement.");
             //handleMessageProtoAck(message);
             break;
+    }
+}
+
+void stop()
+{
+    while(1)
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(50);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(50); 
     }
 }
